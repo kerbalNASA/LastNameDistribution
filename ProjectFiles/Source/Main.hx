5048aa2c2333d357aac98d27306a01e0c06332b3
kerbalNASA <kerbalNASA@users.noreply.github.com>
2015-11-04 04:31:20 +0000

    Update Main.hx

--- ProjectFiles/Source/Main.hx
+++ ProjectFiles/Source/Main.hx
@@ -2,6 +2,7 @@ package;
 
 import openfl.display.*;
 import openfl.events.*;
+import openfl.Lib;
 
 class Main extends Sprite
 {
@@ -12,12 +13,17 @@ class Main extends Sprite
 	public var popTotal:Int = 100000;//total population; 
 	public var numLastName:Int = 100;//number of last names
 	public var lastNameOccurence:Array<Int> = [];//amount of people with each last name
+	public var popDiff:Int;//variable for tracking intial difference in givers/takers in generation
 	
 	var histogramWidth:Int = 600;//histrogram width in pixels
 	var histogramHeight:Int = 300;//histrogram height in pixels
 	var histogramX:Int = 10;//histrogram leftmost X in pixels
 	var histogramY:Int = 10;//histrogram uppermost Y in pixels
-	var graphSprt:Sprite = new Sprite();
+	var gramSprt:Sprite = new Sprite();//sprite for the histrogram
+	var progSprt:Sprite = new Sprite();//sprite for progress bar
+	
+	var barColour:Array<Int> = [];
+	
 	public function new ()
 	{
 		super ();
@@ -38,14 +44,40 @@ class Main extends Sprite
 		{
 			lastNameOccurence[currentGen[i].lastName]++;
 		}
+		initNewGeneration();
+		var stepAmount = Math.ceil(Math.pow(numLastName, 1/3));
+		var stepVal = 200/stepAmount;
+		for(i in 0...numLastName)
+		{
+			barColour.push(createColour(Math.floor(i/(stepAmount*stepAmount))*stepVal, (Math.floor(i/stepAmount)*stepVal)%200,(i*stepVal)%200,255));
+		}
+		drawGraph();
 	}
 	public function drawFrame(e:Event)
+	{
+		drawProgress();
+		if(createNewGeneration(Lib.getTimer()))
+		{
+			drawGraph();
+			initNewGeneration();
+		}
+	}
+	public function drawProgress()
+	{
+		stage.removeChild(progSprt);
+		progSprt = new Sprite();
+		progSprt.graphics.beginFill(0);
+		var ratio:Float = 1-Math.min(givers.length/(popTotal>>1),takers.length/(popTotal>>1));
+		progSprt.graphics.drawRect(histogramX, histogramY+histogramHeight, histogramWidth*ratio, 10);
+		stage.addChild(progSprt);
+	}
+	public function drawGraph()
 	{
 		var sortedA:Array<Int> = lastNameCommonness();
 		var prevV:Int = -1;
 		var prevP:Int = -1;
-		stage.removeChild(graphSprt);
-		graphSprt = new Sprite();
+		stage.removeChild(gramSprt);
+		gramSprt = new Sprite();
 		for(i in 0...sortedA.length)
 		{
 			if(prevV == sortedA[i])
@@ -57,14 +89,13 @@ class Main extends Sprite
 				prevP = lastNameOccurence.indexOf(sortedA[i]);
 				prevV = sortedA[i];
 			}
-			var ratio = sortedA[i]/sortedA[0];
-			graphSprt.graphics.beginFill(prevP*2);
-			graphSprt.graphics.drawRect(histogramX+i*6, histogramY+histogramHeight*(1-ratio), 6, histogramHeight*(ratio));
+			var ratio:Float = sortedA[i]/sortedA[0];
+			gramSprt.graphics.beginFill(barColour[prevP]);
+			gramSprt.graphics.drawRect(histogramX+i*6, histogramY+histogramHeight*(1-ratio), 6, histogramHeight*(ratio));
 		}
-		stage.addChild(graphSprt);
-		createNewGeneration();
+		stage.addChild(gramSprt);
 	}
-	public function createNewGeneration()
+	public function initNewGeneration()
 	{
 		nextGen = [];
 		givers = [];
@@ -80,15 +111,20 @@ class Main extends Sprite
 				takers.push(i);
 			}
 		}
-		var popDiff = Math.abs(givers.length-takers.length);
+		popDiff = Math.round(Math.abs(givers.length-takers.length));
+	}
+	public function createNewGeneration(beginTime:Int):Bool
+	{
 		while(givers.length > 0 && takers.length > 0)
 		{
 			var gPos = Math.floor(givers.length*Math.random());
 			var gCPos = givers[gPos];
-			givers.splice(gPos, 1);
+			givers[gPos] = givers[givers.length-1];
+			givers.pop();
 			var tPos = Math.floor(takers.length*Math.random());
 			var tCPos = takers[tPos];
-			takers.splice(tPos, 1);
+			takers[tPos] = takers[takers.length-1];
+			takers.pop();
 			for(i in 0 ... 2)
 			{
 				nextGen.push(new PClass(Math.floor(2*Math.random()), currentGen[gCPos].lastName));
@@ -98,8 +134,11 @@ class Main extends Sprite
 				nextGen.push(new PClass(Math.floor(2*Math.random()), currentGen[gCPos].lastName));
 				popDiff--;
 			}
+			if(Lib.getTimer()-beginTime>16)
+			return false;
 		}
 		currentGen = nextGen.copy();
+		return true;
 	}
 	public function lastNameCommonness():Array<Int>
 	{
@@ -120,5 +159,24 @@ class Main extends Sprite
 			return 0;
 		} );
 		return sortedOccurence;
+	}
+	public function createColour(red:Float, green:Float, blue:Float, alpha:Float):Int//same as crtClr expect uses floats
+	{
+		return constrain(Math.round(alpha), 0, 255)<<24 | constrain(Math.round(red), 0, 255) << 16 | constrain(Math.round(green), 0, 255) << 8 | constrain(Math.round(blue), 0, 255);
+	}
+	public function constrain(value:Int, min:Int, max:Int):Int // sees if the value (first parameter), is within a minimum value (second parameter) and maximum value (third parameter) and returns the constrained value.
+	{
+		if(value < min)
+		{
+			return min;
+		}
+		else if(value > max)
+		{
+			return max;
+		}
+		else
+		{
+			return value;
+		}
 	}
 }
